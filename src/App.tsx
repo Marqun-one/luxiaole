9e495cc0385ff295df1a0f8ce7fe54691ff1bc6e
Marqun-one <2756538146@qq.com>
2023-10-07 20:28:51 +0800

    新增坚持日历

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <Provider store={store}>
     <BrowserRouter>
       <HomePage></HomePage>
-      </BrowserRouter>
+    </BrowserRouter>
       </Provider>
   );
 }
--- src/views/content/stillon/TimeOut.tsx
+++ src/views/content/stillon/TimeOut.tsx
@@ -1,37 +1,49 @@
-import React from 'react';
-import type { Dayjs } from 'dayjs';
-import type { BadgeProps, CalendarProps } from 'antd';
+import React, { useEffect, useState } from 'react';
+import dayjs, { Dayjs } from 'dayjs';
+import type { BadgeProps, CalendarProps, RadioChangeEvent } from 'antd';
 import { Badge, Calendar } from 'antd';
+import { sendGetRequest, sendPostRequest } from '../../../utils/AxiosUtil';
+import { AddStillOnDetail, StillOnDetail } from '../../../common/publicConfig/Api';
+import { Button, Input, InputNumber, Modal, Space, message } from 'antd';
+import { Radio } from 'antd';
 
-const getListData = (value: Dayjs) => {
+export interface StillOnDetail {
+  id?: number;
+  year: number;
+  month: number;
+  day: number;
+  info: string;
+  status: string;
+}
+
+const plainOptions = ['Apple', 'Pear', 'Orange'];
+const options = [
+  {
+    label: '成功',
+    value: 'success',
+  },
+  {
+    label: '警告',
+    value: 'warning',
+  },
+  {
+    label: '失败',
+    value: 'error',
+  },
+];
+
+const getListData = (value: Dayjs, stillOnDetailList: StillOnDetail[]) => {
   let listData;
-  switch (value.date()) {
-    case 8:
-      listData = [
-        { type: 'warning', content: 'This is warning event.' },
-        { type: 'success', content: 'This is usual event.' },
-      ];
-      break;
-    case 10:
-      listData = [
-        { type: 'warning', content: 'This is warning event.' },
-        { type: 'success', content: 'This is usual event.' },
-        { type: 'error', content: 'This is error event.' },
-      ];
-      break;
-    case 15:
-      listData = [
-        { type: 'warning', content: 'This is warning event' },
-        { type: 'success', content: 'This is very long usual event......' },
-        { type: 'error', content: 'This is error event 1.' },
-        { type: 'error', content: 'This is error event 2.' },
-        { type: 'error', content: 'This is error event 3.' },
-        { type: 'error', content: 'This is error event 4.' },
-      ];
-      break;
-    default:
-  }
-  return listData || [];
+  const year = value.year();
+  const month = value.month() + 1;
+  const day = value.date();
+  let res: { type: string; content: string }[] = []
+  stillOnDetailList.forEach(item => {
+    if (item.year === year && item.month === month && item.day === day) {
+      res = [...res, { type: item.status, content: item.info }]
+    }
+  })
+  return res;
 };
 
 const getMonthData = (value: Dayjs) => {
@@ -40,30 +52,84 @@ const getMonthData = (value: Dayjs) => {
   }
 };
 
+const defaultStillOnDetailList: StillOnDetail[] = [];
+
 const TimeOut: React.FC = () => {
-  //以月为单位进行渲染
+  const today = dayjs()
+  const [renderInfoList, setRenderInfoList] = useState(defaultStillOnDetailList)
+  const [messageApi, contextHolder] = message.useMessage();
+  const [typeValue, setTypeValue] = useState('success');
+  const [open, setOpen] = useState(false);
+  const [curDate, setCurDate] = useState(today)
+  const [info, setInfo] = useState('一次只能增加一条');
+  useEffect(() => {
+    sendGetRequest(StillOnDetail).then((value) => {
+      setRenderInfoList(value.data);
+    })
+  }, []);
+
+  const showModal = () => {
+    setOpen(true);
+  };
+
+   const handleConfirm = async () => {
+    setOpen(false);
+    const stillOnDetailItem: StillOnDetail = {
+      year: curDate.year(),
+      month: curDate.month() + 1,
+      day: curDate.date(),
+      info: info,
+      status: typeValue
+    }
+    console.log(stillOnDetailItem)
+    setRenderInfoList([...renderInfoList, stillOnDetailItem])
+      await sendPostRequest(AddStillOnDetail, stillOnDetailItem).then(response => {
+        const repMsg = response.data;
+        if(repMsg === 'success') {
+          messageApi.open({
+            type: 'success',
+            content: '添加成功!',
+          });
+          return;
+        } 
+        messageApi.open({
+          type: 'error',
+          content: '添加失败!',
+        });
+      });
+
+  };
+
+  const handleCancel = () => {
+    setOpen(false);
+  };
+
+  // 以月为单位进行渲染
   const monthCellRender = (value: Dayjs) => {
     const num = getMonthData(value);
-    return num ? (
+    return (
       <div className="notes-month">
-        {/* <section>{num}</section> */}
-        <span>任务已完成</span>
+        <section>暂未实现</section>
       </div>
-    ) : null;
+    )
   };
 
   // 以日为单位进行渲染
   const dateCellRender = (value: Dayjs) => {
-    const listData = getListData(value);
+    const listData = getListData(value, renderInfoList);
     return (
       <ul className="events">
         {listData.map((item) => (
           <li key={item.content}>
-            <Badge status={item.type as BadgeProps['status']} text={'任务已完成'} />
+            <Badge status={item.type as BadgeProps['status']} text={item.content} />
           </li>
         ))}
       </ul>
     );
+  };
+
+  const handleTypeChange = (value: RadioChangeEvent) => {
+    setTypeValue(value.target.value);
   };
 
   const cellRender: CalendarProps<Dayjs>['cellRender'] = (current, info) => {
@@ -72,10 +138,37 @@ const TimeOut: React.FC = () => {
     return info.originNode;
   };
 
-  return <Calendar cellRender={cellRender} onSelect={(date, info) => {
-    console.log(date);
-    console.log(info);
-  }}/>;
+  return (<div>
+      <div>
+      {contextHolder}
+      </div>
+      <Modal
+        title="详情信息"
+        open={open}
+        onOk={handleConfirm}
+        onCancel={handleCancel}
+        okText="确认"
+        cancelText="取消"
+      >
+        <p><b>日期:</b> {curDate.format('YYYY-MM-DD')}</p>
+        <p><b>今天发生了什么:</b></p>
+        {
+          dateCellRender(curDate)
+        }
+        <p style={{width: 40, height: 30}}>新增: </p>
+        <Space.Compact>
+        <p style={{width: 40, height: 30}}>类型: </p>
+        <Radio.Group options={options} onChange={handleTypeChange} value={typeValue} />
+        </Space.Compact><br/>
+        <p style={{width: 40, height: 30}}>内容: </p>
+        <Input defaultValue={'一次只能增加一条'} style={{height: 30}} onChange={(value) => setInfo(value.currentTarget.value)}/>
+
+      </Modal>
+    <Calendar cellRender={cellRender} onSelect={(date, info) => {
+      setCurDate(date)
+      showModal()
+    }} />;
+  </div>)
 };
 
 export default TimeOut;
